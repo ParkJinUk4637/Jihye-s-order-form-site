2dcf82a7792fee2f2d80890c9b02b9ae4cf31032
pju <noticepju@naver.com>
2023-02-01 20:16:25 +0900

    first commit

---
+++ src/main/java/com/jihye/MarimoOrder/MarimoOrderApplication.java
@@ -0,0 +1,13 @@
+package com.jihye.MarimoOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarimoOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarimoOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jihye/MarimoOrder/MarimoOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihye.MarimoOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarimoOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
